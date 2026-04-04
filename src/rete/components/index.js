f7b3a7e9425f81fbb364d3d4372cbc2f3593df7c
Jason LaFerrera <jason.laferrera@food52.com>
2026-04-04 15:42:02 -0400

    feat: more maths

--- src/rete/components/index.js
+++ src/rete/components/index.js
@@ -1,12 +1,14 @@
-// const fs = require('fs')
-// const files = fs.readdirSync('./')
-// for (const file of files) {
-//   require('./' + file)
-// }
-
 import { AddComponent } from "./AddComponent.jsx";
+import { SubtractComponent } from "./SubtractComponent.jsx";
 import { MultiplyComponent } from "./MultiplyComponent.jsx";
+import { DivideComponent } from "./DivideComponent.jsx";
+import { MaxComponent } from "./MaxComponent.jsx";
 import { MinComponent } from "./MinComponent.jsx";
+import { AbsComponent } from "./AbsComponent.jsx";
+import { ModuloComponent } from "./ModuloComponent.jsx";
+import { RoundComponent } from "./RoundComponent.jsx";
+import { ClampComponent } from "./ClampComponent.jsx";
+import { ScaleComponent } from "./ScaleComponent.jsx";
 import { ConstantComponent } from "./ConstantComponent.jsx";
 import { QuantizerComponent } from "./QuantizerComponent.jsx";
 import { MIDIReceiverComponent } from "./MIDIReceiverComponent.jsx";
@@ -25,8 +27,16 @@ const reteComponents = [
   new MonomeCrowComponent(),
   new ConstantComponent(),
   new AddComponent(),
+  new SubtractComponent(),
   new MultiplyComponent(),
+  new DivideComponent(),
+  new MaxComponent(),
   new MinComponent(),
+  new AbsComponent(),
+  new ModuloComponent(),
+  new RoundComponent(),
+  new ClampComponent(),
+  new ScaleComponent(),
   new QuantizerComponent(),
 ];
 
